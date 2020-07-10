9931dbe7fec5366c050eb73fbf893b0e324965f7
gdutthu <2194454302@qq.com>
2020-07-10 17:27:19 +0800

    面试题41

--- chap5/41_StreamMedian/StreamMedian.py
+++ chap5/41_StreamMedian/StreamMedian.py
@@ -1,29 +1,16 @@
 import random
 
-def StreamMedian(array):
-    pass
-
-
-
-
-
-
-
-
-
-
-
-################大跟堆模块：建立大跟堆、调整大根堆################
-#建立大跟堆
-def heapInsert(array,index):
-    parentIndex=(index-1)//2      #父节点的下标索引
+################大根堆模块：建立大根堆、调整大根堆################
+#建立大根堆
+def bigHeapInsert(array,index):
+    parentIndex=(index-1)//2       #父节点的下标索引
     while parentIndex>=0 and array[parentIndex]<array[index]:
         array[index],array[parentIndex]=array[parentIndex],array[index]
         index=parentIndex
         parentIndex = (index - 1) // 2
 
 #调整大根堆
-def heapIfy(array,index):
+def bigHeapeapIfy(array,index):
     leftSonIndex=2*index+1                          #左右儿子节点的下标索引
     rightSonIndex=2*index+2
     while leftSon<=len(array)-1:                    #大跟堆向下调整的前提是该节点至少存在左儿子节点
@@ -41,21 +28,54 @@ def heapIfy(array,index):
                     index=bigIndex                       #交换后，更新节点信息
                     leftSonIndex = 2 * index + 1         #左右儿子节点的下标索引
                     rightSonIndex = 2 * index + 2
-                else:                                    #若该节点数值比儿子节点的较大值小，则不需要进行交换，直接跳出循环
+                else:                                    #若该节点数值比儿子节点的较大值还大，则不需要进行交换，直接跳出循环
                     break
 
+################小根堆模块：建立小根堆、调整小根堆################
+#建立小跟堆
+def smallHeapInsert(array,index):
+    parentIndex=(index-1)//2             #父节点的下标索引
+    while parentIndex>=0 and array[index]<array[parentIndex]:
+        array[index],array[parentIndex]=array[parentIndex],array[index]
+        index=parentIndex
+        parentIndex = (index - 1) // 2
 
+#调整大根堆
+def smallHeapeapIfy(array,index):
+    leftSonIndex=2*index+1                          #左右儿子节点的下标索引
+    rightSonIndex=2*index+2
+    while leftSon<=len(array)-1:                    #小跟堆向下调整的前提是该节点至少存在左儿子节点
+        if rightSonIndex>len(array)-1:              #该节点只存在左儿子节点
+            if array[leftSonIndex]<array[index]:    #若左儿子节点数值比该节点数值小，就进行交换
+                array[index],array[leftSonIndex]=array[leftSonIndex],array[index]
+            break
+        elif rightSonIndex<=len(array)-1:                #该节点同时存在左右儿子节点
+            if array[leftSonIndex]<=array[rightSonIndex]:#找出左右儿子节点所对应数值的较小值
+                smallIndex=leftSonIndex
+            else:
+                smallIndex=rightSonIndex
+                if array[smallIndex] < array[index]:     #若该节点数值比儿子节点的较小值大，就进行交换
+                    array[index], array[smallIndex] = array[smallIndex], array[index]
+                    index=smallIndex                     #交换后，更新节点信息
+                    leftSonIndex = 2 * index + 1         #左右儿子节点的下标索引
+                    rightSonIndex = 2 * index + 2
+                else:                                    #若该节点数值比儿子节点的较小值还小，则不需要进行交换，直接跳出循环
+                    break
 
-################小跟堆模块：建立小跟堆、调整小根堆################
+def StreamMedian(array):
+    bigHeap=[]                                   #初始化一个大根堆和小根堆
+    smallHeap=[]
 
+    for index in range(len(array)):
 
-
-
-
-
-
-
-
+        if index==0:                            # 一开始先将数据放入大根堆
+            bigHeap.append(array[index])
+        else:                                        #其余情况下
+            if array[index]>bigHeap[0]:              #
+                smallHeap.append(array[index])
+                smallHeapInsert(smallHeap,len(smallHeap)-1)
+            else:
+                bigHeap.append(array[index])
 
 
 
@@ -75,5 +95,8 @@ def randomList(length,low=0,high=100):
 
 if __name__=="__main__":
 
-
-    pass
+    # 采用对数器方法进行对所写代码进行验证,找出数据流的中位数
+    errorCount = 0  # 记录测试过程中算法求解错误的次数
+    for i in range(10000):
+        length = 500                       # 随机数列表的长度
+        array = randomList(length)         # 生成随机数列表
