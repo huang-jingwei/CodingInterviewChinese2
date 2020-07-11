3378977a32b62044a0c67732c20de44fc1b1c830
gdutthu <2194454302@qq.com>
2020-07-11 17:41:59 +0800

    面试题11

--- chap2/11_MinNumberInRotatedArray/MinNumberInRotatedArray.py
+++ chap2/11_MinNumberInRotatedArray/MinNumberInRotatedArray.py
@@ -1,36 +1,65 @@
-import  numpy as np
-import random
 
-#函数功能：判断二维数组中是否含有该整数k
-#解题思路：将该问题转化成求解数组中位数的问题
-#算法复杂度:O(N+M)
 
-def FindInPartiallySortedMatrix(array,k):
-    if array.shape[0]==0 or array.shape[1]==0: #判断数组是否为空数组
+#函数功能：找出旋转数组的最小数字
+#思路一 解题思路：遍历数组,找出数组的最小值
+#算法复杂度:O(N)
+
+def MinNumberInRotatedArray_way1(array):
+    if array==None or len(array)==0:   #判断数组是否为空数组
         return None
-    rowIndex=0                           #初始化移动下标,从数组的右上角开始寻找
-    colIndex=array.shape[1]-1
-    while rowIndex<=array.shape[0]-1 and colIndex>=0:
-        if array[rowIndex,colIndex]==k:  #当前元素数值等于整数k
-            return True
-        elif array[rowIndex,colIndex]>k: #当前元素数较大时，该矩阵为递增矩阵，列位置减1，移动到较小的元素
-            colIndex=colIndex-1
-        elif array[rowIndex,colIndex]<k: #当前元素数较小时，该矩阵为递增矩阵，行位置加1，移动到较大的元素
-            rowIndex=rowIndex+1
-    return False
+    minValue=array[0]                  #初始化数组的最小值
+    for index in range(1,len(array)):
+        if array[index]<minValue:
+            minValue=array[index]
+    return minValue
+
+#函数功能：找出旋转数组的最小数字
+#思路二 解题思路：二分查找
+#算法复杂度:O(log N)
+
+def MinNumberInRotatedArray_way2(array):
+    if array==None or len(array)==0:    #判断数组是否为空数组
+        return None
+    leftIndex=0                               #初始化搜索区间的左右边界下标
+    rightIndex=len(array)-1
+    midIndex=0                                #初始化搜索区间的中间下标
+
+    if array[leftIndex]<array[rightIndex]:    #此时数组并未被旋转，第一个元素就是最小元素
+        return array[0]
+
+    while array[leftIndex]>=array[rightIndex]:
+        if rightIndex-leftIndex==1:           #当两个移动下标索引相邻时，跳出循环
+            midIndex=rightIndex
+            break
+        midIndex=(rightIndex+leftIndex)//2    #更新搜索区间的中间元素的下标
+
+        #如果左右边界、中间点三者数值相等
+        # 此时只能判断最小值在这段搜索区间内，但是无法再进行二分查找了
+        if array[leftIndex]==array[midIndex] and array[midIndex]==array[rightIndex]:
+            return  minSearch(array,leftIndex,rightIndex)
+
+        # 如果该中间元素位于前面的递增子数组，那么它应该大于或者等于第一个指针指向的元素
+        if array[midIndex]>=array[leftIndex]:
+            leftIndex=midIndex
+
+        #如果中间元素位于后面的递增子数组，那么它应该小于或者等于第二个指针指 向的元素
+        elif array[midIndex]<=array[rightIndex]:
+            rightIndex=midIndex
+    return array[midIndex]
+
+#函数功能：针对思路二中无法再进行二分查找的搜索区间，进行顺序查找
+def minSearch(array,index1,index2):
+    minValue=array[index1]
+    for index in range(index1,index2+1):
+        if array[index]<minValue:
+            minValue=array[index]
+    return minValue
 
 if __name__=="__main__":
-    array=np.array([[1,2,8,9],[2,4,9,12],[4,7,10,13]])
-    print(FindInPartiallySortedMatrix(array, k=13))
+    array=[3,4,5,1,2]
+    print(MinNumberInRotatedArray_way1(array))
+    print(MinNumberInRotatedArray_way2(array))
 
-    array = np.array([[0, 1, 2, 3, 4, 5, 6],
-                      [10, 12, 13, 15, 16, 17, 18],
-                      [23, 24, 25, 26, 27, 28, 29],
-                      [44, 45, 46, 47, 48, 49, 50],
-                      [65, 66, 67, 68, 69, 70, 71],
-                      [96, 97, 98, 99, 100, 111, 122],
-                      [166, 176, 186, 187, 190, 195, 200],
-                      [233, 243, 321, 341, 356, 370, 380]])
-    print(FindInPartiallySortedMatrix(array,k=500))
-
-
+    array=[1,0,1,1,1]
+    print(MinNumberInRotatedArray_way1(array))
+    print(MinNumberInRotatedArray_way2(array))
