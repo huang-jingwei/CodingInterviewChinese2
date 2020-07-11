e00b160cb8adce5f69213c187bf67b8068bdc66d
gdutthu <2194454302@qq.com>
2020-07-11 10:15:40 +0800

    面试题3

--- chap2/3_duplicate/duplicate.py
+++ chap2/3_duplicate/duplicate.py
@@ -5,7 +5,7 @@ import random
 #解题思路：将该问题转化成求解数组中位数的问题
 #算法复杂度:O(N)
 
-def MoreThanHalfNum(array):
+def duplicate(array):
     if array==None or len(array)==0:              #判断数组是否为空数组
         return None
     left=0                                        #数组array操作区间左边界下标
@@ -21,28 +21,6 @@ def MoreThanHalfNum(array):
             indexRange = partition(array, left, right)
     return array[mid]
 
-#荷兰国旗问题
-#函数功能：任选数组一个值num，要求把数组中小于num的元素放到数组的左边，大于num的元素放到数组的右边，等于num的元素放到数组的中间
-# 最终返回一个整数数组，其中只有两个值，分别是等于num的数组部分的左右两个下标值。
-#参数说明：array：给定的数组，left：数组操作区间的左边界下标，right：数组操作区间的的右边界下标
-
-def partition(array,left,right):
-    num=array[random.randint(left,right)]  #在数组操作区域区间内任选一个数
-    index=left                             #移动下标
-    less=left-1                            #array数组中，下标≤less的区域存放小于num的数
-    more=right+1                           #array数组中，下标≥more的区域存放大于num的数
-    while index<more:
-        if array[index]==num:
-            index=index+1
-        elif array[index]<num:
-            less=less+1
-            array[index],array[less]=array[less],array[index]
-            index=index+1
-        elif array[index]>num:
-            more=more-1
-            array[index],array[more]=array[more],array[index]
-    indexRange=[less+1,more-1]             #等于num的数组部分的左右两个下标值
-    return indexRange
 
 
 ######################下面代码是测试模块代码##################################
@@ -55,20 +33,27 @@ def randomList(length,low=0,high=100):
         array.append(random.randint(low,high))
     return array
 
+#对照组实验
+def duplicate_right(array):
+    if array==None or len(array)<=0:    #判断数组是否为空数组
+        return None
+    hashMap={}                          #用来记录数组中元素是否是重复元素
+    for index in range(len(array)):
+        if array[index] not in hashMap: #若字典中没存在该元素，则将该元素添加到字典中
+            hashMap[array[index]]=1
+        else:                           #若字典中已经存在该元素，则直接返回
+            return array[index]
+
 if __name__=="__main__":
 
-    array=[1,2,3,2,2,2,5,4,2]          #对教材中的例题进行验证
-    print(MoreThanHalfNum(array))
-
-    # 采用对数器方法进行对所写代码进行验证
+    # 采用对数器方法进行对所写代码进行验证，找出数组中重复的数值
     # 这里验证的时是替代问题，即数组中位数求解是否准确
     errorCount=0                        #记录测试过程中算法求解错误的次数
     for i in  range(10000):
         length=500                      #随机数列表的长度
-        array=randomList(length)        #生成随机数列表
-        sortArray=sorted(array)         #采用库函数对原随机数列表进行升序排序，sorted函数不会改动原数组
-        right=sortArray[(length-1)//2]  #库函数所求的数组中位数
-        test=MoreThanHalfNum(array)     #自己编写的算法所求的中位数
+        array=randomList(length,low=0,high=length) #生成随机数列表
+        right=duplicate_right(array)               #对数器函数，用来做对照组实验
+        test=duplicate(array)                      #自己编写的算法
         if right==test:
             print ("第%d次测试：测试准确"%(i))
         else:
