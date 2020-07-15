18e30cd106557527439c90dc233aca6207b9976b
gdutthu <2194454302@qq.com>
2020-07-15 22:22:34 +0800

    面试题5

--- chap2/5_ReplacesSpaces/ReplacesSpaces.py
+++ chap2/5_ReplacesSpaces/ReplacesSpaces.py
@@ -1,24 +1,6 @@
-# 面试题5：替换空格
+import  numpy as np
+import random
 
-**【题目】：**  请实现一个函数，把字符串 `s` 中的每个空格替换成"%20"。
-
-例如：输入“We are happy.”,则输出““We%20are%20happy.”
-
-
-
-
-
-LeetCode:[替换空格](https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/)
-
-
-
-
-
-【解题思路】
-
-解法一：先将字符串的各个元素都取出，放到数组中。对数组元素进行修改，再将修改后的元素拼接起来。
-
-```python
 #函数功能：把字符串 s 中的每个空格替换成"%20"
 #解法一：利用Python数据结构的特性
 #基本思路：先将字符串的各个元素都取出，放到数组中。对数组元素进行修改，再将修改后的元素拼接起来。
@@ -31,5 +13,10 @@ def ReplacesSpaces_right(s):
     for i in range(len(array)):
         string = string + array[i]
     return string
-```
+
+if __name__=="__main__":
+    s="We are happy."
+    print(s)
+    print(ReplacesSpaces_right(s))
+
 
