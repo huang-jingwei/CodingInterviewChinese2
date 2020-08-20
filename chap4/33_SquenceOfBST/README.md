5052aa139c2f1015da97d31e93f78ad5b0274dd2
gdutthu <2194454302@qq.com>
2020-08-20 11:13:29 +0800

    面试题33

--- chap4/33_SquenceOfBST/README.md
+++ chap4/33_SquenceOfBST/README.md
@@ -34,30 +34,48 @@ LeetCode:[二叉搜索树的后序遍历序列](https://leetcode-cn.com/problems
 
 **解题思路：**
 
-1、对二叉树的后序遍历的序列化结果进行反序列化，构造出原始二叉树；
+1、在解题前先复习基本知识点
 
-2、对上一步所得到的二叉树进行层序遍历，判断是否符合搜索二叉树的要求。
+- 后序遍历的顺序是：左右中
+- 二叉搜索树定义：左子树中所有节点的值 << 根节点的值；右子树中所有节点的值 >> 根节点的值；其左、右子树也分别为二叉搜索树。
 
-
+2、采用递归的方法，将后序遍历结果分为两段，分别来验证准确性
 
 ```Python
-#函数功能：从上到下打印二叉树
-#基本思路：二叉树的层次遍历
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[int]:
-        if root == None:
-            return []
-        data = []       # 队列结构
-        nodeArray = []  # 层次遍历输出数组
-        data.append(root)
-        while len(data) > 0:
-            node = data.pop(0)
-            if node.left != None:
-                data.append(node.left)
-            if node.right != None:
-                data.append(node.right)
-            nodeArray.append(node.val)
-        return nodeArray
+    def verifyPostorder(self, postorder):
+        treeLength = len(postorder)
+        if treeLength == 0 or treeLength == 1:     # 二叉树没有节点或只有一个节点时，为搜索二叉树
+            return True
+        return self.reConstruction(postorder,left=0,right=len(postorder)-1)
+
+
+    #根据后序遍历结果判断二叉树是否是搜索二叉树
+    def reConstruction(self,postorder,left,right):
+        if left >=right:
+            return  True
+        #因为后序遍历的顺序为：左右中
+        #所以后序遍历的尾节点是头结点
+        headVal=postorder[right]
+
+        mid = left-1
+        #搜索二叉树节点数值比左儿子节点数值大，比右儿子节点数值小
+        #找到当前节点的左子树和右子树的分界点
+        while mid+1<=right and postorder[mid+1] < headVal:
+            mid  += 1
+        #若该二叉树为搜索二叉树，那么分界点的数值(除去最后元素）均大于头结点
+        rightIndex=mid
+        while rightIndex+1<len(postorder) and postorder[rightIndex+1]>postorder[right]:
+            rightIndex +=1
+        return  rightIndex==right-1 and self.reConstruction(postorder,mid+1,right-1) and self.reConstruction(postorder,left,mid)
 ```
 
 
--- chap4/33_SquenceOfBST/verifyPostorder.py
+++ chap4/33_SquenceOfBST/verifyPostorder.py
@@ -5,72 +5,30 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
 class Solution:
     def verifyPostorder(self, postorder):
         treeLength = len(postorder)
-        if treeLength == 0 or treeLength == 1:
+        if treeLength == 0 or treeLength == 1:     # 二叉树没有节点或只有一个节点时，为搜索二叉树
             return True
-        head = self.rePostorder(postorder)  # 二叉树的后续遍历反序列化
-        print(treePostPrint(head))
-        nodeData = self.LevelOrder(head)  # 二叉树的层序遍历序列化
-        # 根据二叉树的层序遍历的序列化结果
-        # 判断该二叉树是否是搜索二叉树
-        for index in range(len(nodeData)):
-            if nodeData[index] != None:
-                leftIndex = 2 * index + 1
-                rightIndex = 2 * index + 2
-
-                if leftIndex < len(nodeData) and nodeData[leftIndex] != None and nodeData[index] < nodeData[leftIndex]:
-                    return False
-                if rightIndex < len(nodeData) and nodeData[rightIndex] != None and nodeData[index] > nodeData[
-                    rightIndex]:
-                    return False
-        return True
-
-    # 后序遍历的反序列化
-    def rePostorder(self, postorder):
-        if len(postorder) <= 0:
-            return None
-        val=postorder.pop()
-        root=None
-        if val != '#':
-            root = TreeNode(val)
-            root.right = self.rePostorder(postorder)
-            root.left = self.rePostorder(postorder)
-        return root
-
-    # 二叉树的层序遍历的序列化
-    def LevelOrder(self, head):
-        queue = []  # 层序遍历的辅助队列
-        stack = []  # 层序遍历得到的二叉树节点
-        queue.append(head)
-        while len(queue) > 0:
-            node = queue.pop(0)
-
-            if node != None:
-                stack.append(node.val)
-                queue.append(node.left)
-                queue.append(node.right)
-            else:
-                stack.append(None)
-        # 去除叶节点的左右儿子节点
-        while len(stack) > 0 and stack[-1] == None:
-            stack.pop()
-        print(stack)
-        return stack
-
-def treePostPrint(root):
-    array=""
-    if root==None:
-        array=array+"#!"
-    else:
-        array=array+treePostPrint(root.left)
-        array=array+treePostPrint(root.right)
-        array=array+str(root.val)+"!"
-    return array
+        return self.reConstruction(postorder,left=0,right=len(postorder)-1)
 
 
-postorder=[4, 8, 6, 12, 16, 14, 10]
-postorder=[1,6,3,2,5]
-test=Solution()
-print(test.verifyPostorder(postorder))
+    #根据后序遍历结果判断二叉树是否是搜索二叉树
+    def reConstruction(self,postorder,left,right):
+        if left >=right:
+            return  True
+        #因为后序遍历的顺序为：左右中
+        #所以后序遍历的尾节点是头结点
+        headVal=postorder[right]
+
+        mid = left-1
+        #搜索二叉树节点数值比左儿子节点数值大，比右儿子节点数值小
+        #找到当前节点的左子树和右子树的分界点
+        while mid+1<=right and postorder[mid+1] < headVal:
+            mid  += 1
+        #若该二叉树为搜索二叉树，那么分界点的数值(除去最后元素）均大于头结点
+        rightIndex=mid
+        while rightIndex+1<len(postorder) and postorder[rightIndex+1]>postorder[right]:
+            rightIndex +=1
+        return  rightIndex==right-1 and self.reConstruction(postorder,mid+1,right-1) and self.reConstruction(postorder,left,mid)
