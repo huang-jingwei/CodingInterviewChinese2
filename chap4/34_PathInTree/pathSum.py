551a33e6ac74abd7cca2dd35cc5a9ab37df75c25
gdutthu <2194454302@qq.com>
2020-08-21 10:45:46 +0800

    面试题34

--- chap4/34_PathInTree/pathSum.py
+++ chap4/34_PathInTree/pathSum.py
@@ -1,26 +1,43 @@
-# Definition for a Node.
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+
+import copy
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        if head==None :             #输入节点是空节点,z直接返回空节点
-            return None
+    def __init__(self):
+        self.treePath = []            # 用来存放可行解集
 
-        #将nodeData= {None:None}进行这样子的初始化，其实就是先将最后的空节点压入
-        #否则在最后的重构链表节点时候，找不到空节点进行连接
-        nodeData= {None:None}      #用字典结构存放节点信息
-        node=head
-        while node :
-            nodeData[node]=Node(node.val)
-            node=node.next
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        if root == None:
+            return []
+        targetSum = sum
+        path = []
+        self.findPath(root, targetSum, path)
+        return self.treePath
 
-        node=head                  #连接节点，重构链表
-        while node:
-            nodeData[node].next=nodeData[node.next]
-            nodeData[node].random=nodeData[node.random]
-            node=node.next
-        return nodeData[head]
+    def findPath(self, root, targetSum, path):
+        path.append(root.val)
+
+        # 因为二叉树的路径是根节点到叶子节点
+        # 判断当前节点是否是叶子节点
+        isLeaf = False
+        if root.left == None and root.right == None:
+            isLeaf = True
+
+        # 当前路径符合要求时，把当前路径添加到可行解集中
+        # 在添加路径时，要采用copy方法复制一次，因为Python是动态列表
+        # 否则在下列代码的代码中，path进行弹出元素时，可行解集也会弹出元素
+        if isLeaf == True and sum(path) == targetSum:
+            self.treePath.append(copy.copy(path))
+
+        # 不是叶子节点时，继续遍历它的儿子节点
+        if root.left != None:
+            self.findPath(root.left, targetSum, path)
+        if root.right != None:
+            self.findPath(root.right, targetSum, path)
+        # 返回父节点前，在路径上删除当前节点
+        path.pop()
